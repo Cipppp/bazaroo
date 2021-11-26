0a085c1c17b8bfeff98211bbdffa3a8d2f8fade4
Cipppp <pirvuciprian37@gmail.com>
2021-11-26 17:54:27 +0200

    Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './styles/output.css';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div class="bg-red w-full"></div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Button() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+
+export default Button
