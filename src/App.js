6f5dea74c07f59d0dfc7b0d4e2d6e1e6377aa653
Cipppp <pirvuciprian37@gmail.com>
2021-11-28 23:04:07 +0200

    404 page updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
     Login,
     Register,
 } from './pages';
-import { BrowserRoute as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
